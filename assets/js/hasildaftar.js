2f3010b53e41939eaab86db5d56bb0a94775b061
Siti Kamila Tunawa <125983493+kamilanawa@users.noreply.github.com>
2023-10-10 15:38:54 +0700

    Update hasildaftar.js logic menampilkan hasil daftar

--- assets/js/hasildaftar.js
+++ assets/js/hasildaftar.js
@@ -1,26 +1,29 @@
- const hasilBooking = document.getElementById("hasilBooking");
-      const apiUrl = "https://be-semarang-16-production.up.railway.app/course";
+// Define a function to fetch and display booking information
+function fetchAndDisplayBooking() {
+  const hasilBooking = document.getElementById("hasilBooking");
+  const apiUrl = "https://be-semarang-16-production.up.railway.app/course";
 
-      fetch(apiUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then((data) => {
-          // Handle the retrieved data here
-          // Modify the code below to match your database structure
-          const lastBooking = data[data.length - 1];
-          const bookingDetails = `
-            <h2>Booking Information</h2>
-            <p><span class="booking-label">CourseID</span><span class="booking-value">${lastBooking.course_id}</span></p>
-            <p><span class="booking-label">Course Name:</span><span class="booking-value">${lastBooking.course_name}</span></p>
-            <p><span class="booking-label">Course Category:</span><span class="booking-value">${lastBooking.course_category}</span></p>
-            <p><span class="booking-label">Course level:</span><span class="booking-value">${lastBooking.course_level}</span></p>
-          `;
-          hasilBooking.innerHTML = bookingDetails;
-        })
-        .catch((error) => {
-          console.error("Fetch error:", error);
-        });
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const lastBooking = data[data.length - 1];
+      const bookingDetails = `
+        <h2>Booking Information</h2>
+        <p><span class="booking-label">CourseID</span><span class="booking-value">${lastBooking.course_id}</span></p>
+        <p><span class="booking-label">Course Name:</span><span class="booking-value">${lastBooking.course_name}</span></p>
+        <p><span class="booking-label">Course Category:</span><span class="booking-value">${lastBooking.course_category}</span></p>
+        <p><span class="booking-label">Course level:</span><span class="booking-value">${lastBooking.course_level}</span></p>
+      `;
+      hasilBooking.innerHTML = bookingDetails;
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
+
+fetchAndDisplayBooking();
